caf1a21a39ee25db27779c56490817e32c754781
kongweichen <kongweichen@kuaishou.com>
2025-05-30 18:07:53 +0800

    docs. add a todo file

---
+++ Pods/Target Support Files/Pods-weatherSearcher-weatherSearcherUITests/Pods-weatherSearcher-weatherSearcherUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weatherSearcher_weatherSearcherUITests : NSObject
+@end
+@implementation PodsDummy_Pods_weatherSearcher_weatherSearcherUITests
+@end
---
+++ Pods/Target Support Files/Pods-weatherSearcher/Pods-weatherSearcher-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weatherSearcher : NSObject
+@end
+@implementation PodsDummy_Pods_weatherSearcher
+@end
