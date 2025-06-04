fabb3c1d0ffe964e1de2198b2013044f6f0ce95d
kongweichen <kongweichen@kuaishou.com>
2025-06-04 11:18:08 +0800

    chore. clean repo

--- Pods/Target Support Files/Pods-weatherSearcher-weatherSearcherUITests/Pods-weatherSearcher-weatherSearcherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_weatherSearcher_weatherSearcherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_weatherSearcher_weatherSearcherUITests
-@end
--- Pods/Target Support Files/Pods-weatherSearcher/Pods-weatherSearcher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_weatherSearcher : NSObject
-@end
-@implementation PodsDummy_Pods_weatherSearcher
-@end
